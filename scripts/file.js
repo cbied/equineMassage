222604abd82e5c60398908660a9363983ccdf3dc
Christopher Biediger <cabiediger@gmail.com>
2019-01-13 16:50:24 +0700

    finished structure of site, work on modal mission statement and get buttons/links working

--- scripts/file.js
+++ scripts/file.js
@@ -1,4 +1,4 @@
-// Navbar
+// Navbar load
 ( function( $ ) {
     $( document ).ready(function() {
     if(window.innerWidth > 560) {
@@ -14,6 +14,7 @@
     });
     } )( jQuery );
 
+  // menu #navbarIcon toggle
     function menuChange(x) {
         x.classList.toggle("change");
         if($('div').hasClass('show') == true) {
@@ -23,6 +24,29 @@
         }
       }
 
+    // fixed menu when FAQ collapse is open
+    $( '#cNav' ).on('click', function(){
+        $('#collapse1, #collapse2, #collapse3').removeClass('show');
+        if($('div').hasClass('show') == true) {
+            $('#navbarIcon').css('display', 'none');
+        } else if($('div').hasClass('show') == false){
+            $('#navbarIcon').css('display', 'block');
+        }
+    });
+
+    // hide menu when a link in menu is clicked
+    $('#mobleNav a').on('click', function() {
+        $('.navbar-collapse').toggleClass('show')
+        $('.navbar-collapse').toggle('.show')
+        if($('.containerNav').hasClass('change')) {
+            $('.containerNav').removeClass('change')
+            $('.containerNav').addClass('collapsed')
+            $('.containerNav').attr("aria-expanded","false")
+        }
+       
+      })
+
+    // menu change when resize window
     $( window ).resize(function() {
         if(window.innerWidth > 560) {
             $('#cssmenu, #bg-one, #bg-two, #bg-three, #bg-four').css('display', 'block');
@@ -39,3 +63,24 @@
         }
     });
 
+   
+
+// Ad smooth scrolling
+$('#cssmenu a, #mobleNav a').on('click', function(defultEvent) {
+  // Check for a hash value
+  if(this.hash !== '') {
+    // Prevent defult behavior
+    defultEvent.preventDefault();
+
+    // Store hash
+    const hash = this.hash;
+
+    // animate smooth scrolling
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function() {
+      // Add hash to URL after scroll
+      window.location.hash = hash;
+  });
+  }
+});
